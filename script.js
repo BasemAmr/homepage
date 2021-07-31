dffd105aed10be29a5c42e7e11e5f1e89bb45cf9
BasemAmr <67838239+BasemAmr@users.noreply.github.com>
2021-07-31 22:09:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ function myFunction() {
 }
 
 window.addEventListener('resize', function(){
+  let iframe = document.querySelector("iframe");
+  let map = document.querySelector("#map");
   if (window.innerWidth > 768) {
-    console.log(window.innerWidth);
     x.className = "nvbr";
+    iframe.width = 450
+    map.style.padding = `2.5rem 0`
+  } else {
+    iframe.width = window.innerWidth - (window.innerWidth * .15)
+    map.style.padding = `2.5rem 0.5rem`
   }
 });
