83d400de12f4ee4944f24f589d4725cabcab8c00
BasemAmr <1.million.arab.coders@gmail.com>
2022-01-17 01:23:40 +0200

    Updating Portfolio

--- script.js
+++ script.js
@@ -6,16 +6,3 @@ function myFunction() {
     x.className = "nvbr";
   }
 }
-
-window.addEventListener('resize', function(){
-  let iframe = document.querySelector("iframe");
-  let map = document.querySelector("#map");
-  if (window.innerWidth > 768) {
-    x.className = "nvbr";
-    iframe.width = 450
-    map.style.padding = `2.5rem 0`
-  } else {
-    iframe.width = window.innerWidth - (window.innerWidth * .15)
-    map.style.padding = `2.5rem 0.5rem`
-  }
-});
